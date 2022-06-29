13d5c90a3e0b3dbb284d5b4e1b006b2ea557abad
Umi <julianogrady07@gmail.com>
2022-06-29 15:30:21 -0400

    added temp buildfiles to gitignore

--- build/Input.d
+++
@@ -1,6 +0,0 @@
-Input.o: \
- C:/Users/julia/OneDrive/Desktop/Code/sdl2/tintinn/source/Input.cpp \
- C:/Users/julia/OneDrive/Desktop/Code/sdl2/tintinn/source/include/Input.hpp \
- C:/Users/julia/OneDrive/Desktop/Code/sdl2/tintinn/source/include/types.hpp
-C:/Users/julia/OneDrive/Desktop/Code/sdl2/tintinn/source/include/Input.hpp:
-C:/Users/julia/OneDrive/Desktop/Code/sdl2/tintinn/source/include/types.hpp:
